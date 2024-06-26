db22764f625b510b7a8dfacee11bf217f4db05ed
ayshugct <ayshu.gct@gmail.com>
2024-06-26 21:09:05 +0530

    Initial Routing

--- angular-examples/form-example/src/app/app.routes.ts
+++ angular-examples/form-example/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { EmployeeComponent } from './component/employee/employee.component';
+import { AdminComponent } from './component/admin/admin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: EmployeeComponent},
+    {path:'employee', component:EmployeeComponent},
+    {path:'admin', component:AdminComponent},
+    {path:'**', component: EmployeeComponent}
+];
