b237abf29671c465f4d73014949c017a3c87dd29
ayshugct <ayshu.gct@gmail.com>
2024-06-27 12:26:23 +0530

    Single SPA Angular with Navbar and Main app

---
+++ angular-examples/single-spa-angular/navbar/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule]  
+
+})
+export class AppComponent {
+  title = 'nabar';
+}
